19fcb4be2daa6011d7da1fa623d1f40708432ceb
Suraj0950 <surajsuryaveer@gmail.com>
2025-09-22 11:30:35 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className=' bg-amber-200 flex items-center justify-center'>Welcome to notesBuddy Applicaton</h1>
+      <h1 className=' bg-amber-200 flex items-center justify-center h-[50px] w-[full]'>Welcome to notesBuddy Application</h1>
     </div>
   )
 }
